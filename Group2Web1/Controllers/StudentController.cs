a20c9ead87fc6b792753b9f90e51aeddda897426
Nusret Bilallaj <netktit@gmail.com>
2022-11-10 18:24:15 +0100

    Eshte shtuar top studenti

--- Group2Web1/Controllers/StudentController.cs
+++ Group2Web1/Controllers/StudentController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Group2Web1.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Group2Web1.Controllers
 {
@@ -7,6 +8,13 @@ namespace Group2Web1.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult ParaqitTopStudentin()
+        {
+            Studenti stud1 = new Studenti("Kastriot", "Gjoka", 20);
+            
+            return View(stud1);
         }
     }
 }
---
+++ Group2Web1/Models/Studenti.cs
@@ -0,0 +1,21 @@
+﻿namespace Group2Web1.Models
+{
+    public class Studenti
+    {
+        public Studenti(string emri,string mbiemri,int mosha)
+        {
+            Emri = emri;
+            Mbiemri = mbiemri;
+            Mosha = mosha;
+        }
+        public string Emri { get; set; }
+        public string Mbiemri { get; set; }
+        public int Mosha { get; set; }
+
+        public string SayHello()
+        {
+            return $"Tungjatjeta {Emri} {Mbiemri}";
+        }
+
+    }
+}
